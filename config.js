d7f1d7720259fef3622a51b1e3807e35b275ea75
Anup930 <Anup.gretex@gmail.com>
2025-12-17 11:11:58 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // APPS SCRIPT URL YAHAN PASTE KARO
-const API_URL = "YOUR_DEPLOYED_WEB_APP_URL_HERE"; 
+const API_URL = "https://script.google.com/macros/s/AKfycbxM4m4g4cgeKXSA2CF6uhJ42OJuYgdYr-JRFmqTbqH7_p51VAWrk9hoY6IWhE8gE1bMdQ/exec"; 
 
 function checkSession(role) {
     const userRole = sessionStorage.getItem('userRole');
