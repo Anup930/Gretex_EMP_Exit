244e5ef2faae65c2ef73fbae4b8bb28b61dffbd1
Anup930 <Anup.gretex@gmail.com>
2025-12-17 11:08:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,12 @@
-// config.js
-const API_URL = "YOUR_GOOGLE_APPS_SCRIPT_DEPLOYMENT_URL_HERE";
+// APPS SCRIPT URL YAHAN PASTE KARO
+const API_URL = "YOUR_DEPLOYED_WEB_APP_URL_HERE"; 
 
-// Ye check karega ki banda login hai ya nahi
-function checkSession(requiredRole) {
+function checkSession(role) {
     const userRole = sessionStorage.getItem('userRole');
     if (!userRole) {
-        window.location.href = 'index.html'; // Login nahi hai to bhaga do
-    } else if (requiredRole && userRole !== requiredRole) {
-        alert("Access Denied! Aap is department ke nahi ho.");
+        window.location.href = 'index.html';
+    } else if (role && userRole !== role) {
+        alert("Access Denied! Wrong Department.");
         window.location.href = 'index.html';
     }
 }
